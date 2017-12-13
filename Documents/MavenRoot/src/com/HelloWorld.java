496a06ea854710fd5c49f0e45d15ae5868697b23
Karthik Holikatti <Karthik.Holikatti@oneadvanced.com>
2017-12-13 15:21:58 +0530

    MSD

---
+++ Documents/MavenRoot/src/com/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+	  String a= "Hello";
+	  String b="World";
+    	Add(a, b);
+	
+  }
+  
+  public static void Add(String a, String b)
+{
+	String Result;
+	Result=a+b;
+	System.out.println("Concatenated String is=" + Result);
+}
+}
+
+
+ 
